353f3e3813c8e061f1f08ee0527e00a599c009e5
jramian-apprentissage <j.ramian@cepremium.fr>
2026-02-26 00:39:15 +0100

    Create types.ts

---
+++ lib/types.ts
@@ -0,0 +1,34 @@
+export type Category = {
+  id: string
+  household_id: string
+  name: string
+  icon: string
+  color: string
+  type: 'income' | 'expense'
+  is_archived: boolean
+  created_at: string
+
+  // quand tu fais .select('*, subcategories(*)')
+  subcategories?: SubCategory[]
+}
+
+export type SubCategory = {
+  id: string
+  category_id: string
+  name: string
+  is_archived: boolean
+  created_at: string
+}
+
+export type Transaction = {
+  id: string
+  household_id: string
+  user_id: string
+  date: string // YYYY-MM-DD
+  type: 'income' | 'expense'
+  amount: number
+  category_id: string
+  subcategory_id: string | null
+  note: string | null
+  created_at: string
+}
